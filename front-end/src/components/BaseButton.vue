e92adff6a5f8d69ff957dc22770ce412a29f709f
Ranie <143940800+ranyesantos@users.noreply.github.com>
2024-08-16 21:53:24 -0300

    added views login and register

---
+++ front-end/src/components/BaseButton.vue
@@ -0,0 +1,41 @@
+<template>
+
+    <button :type="type" class="base-button">
+        <slot></slot>
+    </button>
+
+</template>
+
+<script>
+
+    export default {
+        props: {
+            type: {
+            type: String,
+            default: 'button',
+            },
+        },
+    }
+
+</script>
+
+<style scoped>
+
+    .base-button {
+    width: 490px;
+    height: 62px;
+    border-radius: 30px;
+    background-color: rgb(5, 63, 170);
+    color: rgb(255, 255, 255);
+    font-size: 24px;
+    border: none;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+    }
+
+    .base-button:hover {
+    background-color: rgb(4, 50, 135); 
+    }
+
+</style>
+  
---
+++ front-end/src/views/LoginView.vue
@@ -0,0 +1,137 @@
+<template>
+    <div class="main">
+        <div class="login-container">
+            <h1>Acesse a sua conta</h1>
+            <form>
+
+                <BaseInput
+                    type="email"
+                    placeholder="E-mail"
+                    v-model="email"
+                    class="baseinput"
+                />
+                <BaseInput
+                    type="password"
+                    placeholder="Senha"
+                    v-model="password"
+                    class="baseinput"
+                />
+
+                <BaseButton type="submit">Entrar</BaseButton>
+            </form>
+
+            <p>Não tem uma conta? <a @click="registerRedirect">Registre-se</a></p>
+
+        </div>
+    </div>
+</template>
+
+
+<script>
+    import BaseInput from '../components/BaseInput.vue';
+    import BaseButton from '../components/BaseButton.vue';
+    import { useRouter } from 'vue-router';
+
+    export default {
+        components: {
+            BaseInput,
+            BaseButton,
+        },
+        setup() {
+            const router = useRouter(); // Obtém o roteador
+
+            const registerRedirect = () => {
+                router.push('/register'); // Navega para a rota /register
+            };
+
+            return {
+                registerRedirect
+            };
+        }
+    }
+</script>
+
+
+<style>
+   .main {
+    font-family: 'Roboto', 'sans-serif';
+    display: flex;
+    justify-content: center;
+    height: 100vh;
+    align-items: center;
+    background-color: rgb(5, 63, 170);
+    
+}
+
+.login-container {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    background-color: rgb(255, 255, 255);
+    width: 580px;
+    height: 550px;
+    box-shadow: 0 4px 8px 2px rgba(26, 25, 25, 0.274);
+    border-radius: 2.5px;
+}
+
+
+form {
+    display: flex;
+    flex-direction: column; 
+    justify-content: center;
+    align-items: center; 
+    width: 100%;
+    margin-top: 40px;
+}
+
+
+
+p {
+    font-size: 16px;
+    color: rgb(56, 56, 56);
+    margin-top: 20px;
+    text-align: center;
+}
+
+a {
+    color: rgb(45, 117, 252);
+    text-decoration: none;
+    font-weight: bold;
+    background-color: transparent;
+    transition: color 0.3s ease, text-decoration 0.3s ease;
+}
+
+a:hover {
+    text-decoration: underline;
+    color: rgb(9, 87, 231);
+    background-color: transparent;
+}
+
+@media (max-width: 650px) {
+    .login-container {
+        width: 60%;
+        max-width: 100%;
+        margin-top: 0;
+        padding: 10px;
+    }
+
+    input {
+        width: 100%;
+        height: 50px;
+        margin-bottom: 15px;
+        font-size: 14px;
+    }
+
+    button {
+        width: 100%;
+        height: 50px;
+        font-size: 16px;
+    }
+
+    p {
+        font-size: 14px;
+        margin-top: 10px;
+    }
+}
+</style>
